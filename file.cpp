8ad09176059472d9c3fb92d0e36f282f4326b454
hhuziuk <huziukwork@gmail.com>
2024-01-15 23:59:32 +0100

    based commit02

--- file.cpp
+++ file.cpp
@@ -3,13 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-// XOR Encryption/Decryption
+//////////////////////////////////////////////////////////////////////////////////
+/////////////////////////XOR Encryption/Decryption////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+// Szyfr XOR to algorytm szyfrowania, który wykorzystuje słowo kluczowe jako klucz i może być zapisany w następujący sposób
+// Ci = Pi XOR Kj, gdzie Kj jest j-tą literą słowa kluczowego reprezentowaną w kodowaniu ASCII.
+//
+// Jeśli użyjesz klucza, który jest co najmniej tak długi, jak długość wiadomości, 
+// szyfr XOR staje się znacznie silniejszy kryptograficznie niż w przypadku użycia powtarzającego się klucza. 
+// Jeśli do wygenerowania takiego klucza używany jest generator liczb pseudolosowych, wynikiem jest szyfr strumieniowy.
+// Jeśli do wygenerowania klucza zostanie użyty prawdziwie losowy generator liczb, wówczas mamy do czynienia z szyfrem Vernama, 
+// jedynym systemem kryptograficznym, dla którego teoretycznie udowodniono absolutną siłę kryptograficzną.
+//
+
+// przykład szyfru XOR zaimplementowanego w języku C
 void xorEncryptDecryptC(uint8_t* data, size_t len, uint8_t key) {
     for (size_t i = 0; i < len; i++) {
         data[i] ^= key;
     }
 }
 
+// przykład szyfru XOR zaimplementowanego w ASM
 void xorEncryptDecryptASM(uint8_t* data, size_t len, uint8_t key) {
     __asm {
         mov ecx, len
@@ -23,13 +37,113 @@ void xorEncryptDecryptASM(uint8_t* data, size_t len, uint8_t key) {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////Szyfr podstawieniowy/////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// Szyfr podstawieniowy to rodzaj algorytmu szyfrowania, w którym każdy znak tekstu jawnego jest zastępowany innym znakiem za pomocą klucza.
+// Klucz definiuje jednoznaczne przyporządkowanie między znakami tekstu jawnego a odpowiadającymi im znakami zaszyfrowanymi.
+// Istnieją różne rodzaje szyfrów podstawieniowych, takie jak szyfr Cezara, szyfr Vigenère'a, czy szyfr jednopodstawieniowy.
+
+// Szyfr działa na zasadzie zamiany każdego znaku tekstu jawnego na odpowiadający mu znak zaszyfrowany według ustalonego klucza.
+// To sprawia, że znak ten staje się nieczytelny dla osób, które nie posiadają klucza szyfrowania.
+// Szyfry podstawieniowe są stosunkowo proste do zrozumienia i zaimplementowania, co sprawia, że są używane w edukacji i do celów demonstracyjnych.
+
+// przykład szyfru podstawieniowy zaimplementowanego w C
+void substitutionEncryptC(char* data, size_t len,
+    const uint8_t* substitutionTable) {
+    for (size_t i = 0; i < len; i++) {
+        data[i] = substitutionTable[(uint8_t)data[i]];
+    }
+}
+
+// przykład szyfru podstawieniowy zaimplementowanego w C
+void substitutionDecryptC(char* data, size_t len,
+    const uint8_t* substitutionTable) {
+    for (size_t i = 0; i < len; i++) {
+        uint8_t substitutedValue = data[i];
+
+        for (uint8_t j = 0; j < 256; j++) {
+            if (substitutionTable[j] == substitutedValue) {
+                data[i] = j;
+                break;
+            }
+        }
+    }
+}
+
+// przykład szyfru podstawieniowy zaimplementowanego w ASM
+void substitutionEncryptASM(char* data, size_t len,
+    const uint8_t* substitutionTable) {
+    __asm {
+        mov ecx, len
+        mov esi, data
+        mov edi, substitutionTable
+
+        substitution_encrypt_loop :
+        movzx eax, byte ptr[esi]
+            movzx eax, byte ptr[edi + eax]
+            mov byte ptr[esi], al
+
+            inc esi
+            loop substitution_encrypt_loop
+    }
+}
+
+// przykład szyfru podstawieniowy zaimplementowanego w ASM
+void substitutionDecryptASM(char* data, size_t len,
+    const uint8_t* substitutionTable) {
+    __asm {
+        mov ecx, len
+        mov esi, data
+        mov edi, substitutionTable
+
+        substitution_decrypt_loop :
+        movzx eax, byte ptr[esi]
+            xor edx, edx
+
+            search_in_table :
+        cmp byte ptr[edi + edx], al
+            je found
+            inc edx
+            cmp edx, 256
+            jl search_in_table
+
+            found :
+        mov byte ptr[esi], dl
+
+            inc esi
+            loop substitution_decrypt_loop
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////Caesar Encryption/Decryption////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+// Szyfr Cezara – jedna z najprostszych technik szyfrowania. 
+// Jest to rodzaj szyfru podstawieniowego, w którym każda litera tekstu niezaszyfrowanego zastępowana jest inną, oddaloną od niej o stałą liczbę pozycji w alfabecie, 
+// literą (szyfr monoalfabetyczny), przy czym kierunek zamiany musi być zachowany. Nie rozróżnia się przy tym liter dużych i małych.
+//
+// Zasada działania polega na cyklicznym przesuwaniu alfabetu, a kluczem jest liczba liter, o które następuje przesunięcie.
+// 
+// Ogólny wzor:
+// y=(x+k)mod\n
+// x=(y-k)mod n, gdzie
+// x jest numerem sekwencyjnym znaku tekstu jawnego, 
+// y - numer porządkowy znaku szyfrogramu, 
+// n - potęga alfabetu 
+// k - klucz
+// Szyfr Cezara ma zbyt mało kluczy - o jeden mniej niż liter w alfabecie. Dlatego też łatwo jest go złamać metodą brute-force - 
+// próbując wszystkich możliwych kluczy, aż do odszyfrowania rozpoznawalnego tekstu jawnego.
+
+// przykład szyfru Cezara zaimplementowanego w języku C
 void caesarEncryptDecryptC(uint8_t* data, size_t len, uint8_t shift) {
     for (size_t i = 0; i < len; i++) {
         data[i] = (data[i] + shift) % 256;
     }
 }
 
-// Caesar Encryption/Decryption
+// przykład szyfru Cezara zaimplementowanego w języku ASM
 void caesarEncryptDecryptASM(uint8_t* data, size_t len, int8_t shift) {
     __asm {
         mov ecx, len
@@ -50,6 +164,26 @@ void caesarEncryptDecryptASM(uint8_t* data, size_t len, int8_t shift) {
     }
 }
 
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////Tiny Encryption Algorithm///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+// TEA (Tiny Encryption Algorithm) – symetryczny szyfr blokowy opracowany przez Rogera Needhama i Davida Wheelera w 1994 roku.
+// Szyfr oparty jest o Sieć Feistela i wykorzystuje operacje z mieszanych grup algebraicznych. 
+// Cechuje się małą zajętością pamięci, dużą szybkością szyfrowania, wysoką odpornością na kryptoanalizę różnicową i zdolnością do pełnej dyfuzji już po sześciu rundach.
+//
+// Algorytm szyfrowania TEA opiera się na operacjach bitowych z 64-bitowym blokiem i ma 128-bitowy klucz szyfrowania. 
+// Standardowa liczba rund sieci Feistel wynosi około 64 (32 cykle), jednak aby osiągnąć najlepszą wydajność lub szyfrowanie, 
+// liczbę cykli można zmieniać od 8 (16 rund) do 64 (128 rund).
+//
+// Zaletami szyfru są prostota implementacji, niewielki rozmiar kodu i dość duża szybkość wykonania, a także możliwość optymalizacji wykonania na standardowych 
+// procesorach 32-bitowych, ponieważ główne operacje to wyłączne LUB, przesunięcie bitowe i dodawanie modulo 232. Ponieważ algorytm nie wykorzystuje żadnych tabel podstawień, 
+// a funkcja rundy jest dość prosta, algorytm wymaga co najmniej 16 cykli (32 rundy), aby osiągnąć efektywną dyfuzję, chociaż pełna dyfuzja jest osiągana w 6 cyklach (12 rund).
+//
+// Algorytm ma doskonałą odporność na kryptoanalizę liniową i wystarczająco dobrą odporność na kryptoanalizę różnicową. 
+// Główną wadą tego algorytmu szyfrowania jest jego podatność na ataki z użyciem powiązanych kluczy.
+
+// przykład szyfru TEA zaimplementowanego w C
 void tea_encrypt_c(uint32_t msg[2], const uint32_t key[4]) {
     uint32_t y = msg[0];
     uint32_t z = msg[1];
@@ -69,6 +203,7 @@ void tea_encrypt_c(uint32_t msg[2], const uint32_t key[4]) {
     msg[1] = z;
 }
 
+// przykład szyfru TEA zaimplementowanego w ASM
 void tea_encrypt_ASM(uint32_t msg[2], const uint32_t key[4]) {
     __asm {
         // Input parameters
@@ -119,6 +254,7 @@ void tea_encrypt_ASM(uint32_t msg[2], const uint32_t key[4]) {
     }
 }
 
+// przykład szyfru TEA zaimplementowanego w C
 void tea_decrypt_c(uint32_t msg[2], const uint32_t key[4]) {
     uint32_t y = msg[0];
     uint32_t z = msg[1];
@@ -138,6 +274,7 @@ void tea_decrypt_c(uint32_t msg[2], const uint32_t key[4]) {
     msg[1] = z;
 }
 
+// przykład szyfru TEA zaimplementowanego w ASM
 void tea_decrypt_ASM(uint32_t msg[2], const uint32_t key[4]) {
     __asm {
         // Input parameters
@@ -185,6 +322,19 @@ void tea_decrypt_ASM(uint32_t msg[2], const uint32_t key[4]) {
     }
 }
 
+
+
+//////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////Szyfr Vigenère’a/////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Szyfr Vigenère'a to szyfr polialfabetyczny, który wykorzystuje słowo jako klucz.
+// Ci = (Pi + Kj) mod 33, gdzie Kj jest j-tą literą słowa kluczowego, Pi jest i-tą literą słowa źródłowego.
+// Należy on do grupy tzw. polialfabetycznych szyfrów podstawieniowych.
+// Każdy z wierszy tablicy odpowiada szyfrowi Cezara, przy czym w pierwszym wierszu przesunięcie wynosi 0, w drugim 1 itd.
+// Aby zaszyfrować pewien tekst, potrzebne jest słowo kluczowe. Słowo kluczowe jest tajne i mówi, z którego wiersza (lub kolumny) należy w danym momencie skorzystać.
+
+// przykład szyfru Vigenère’a zaimplementowanego w C
 void vigenereEncryptC(char* data, size_t len, const char* key) {
     size_t keyLen = strlen(key);
     for (size_t i = 0; i < len; i++) {
@@ -192,6 +342,7 @@ void vigenereEncryptC(char* data, size_t len, const char* key) {
     }
 }
 
+// przykład szyfru Vigenère’a zaimplementowanego w ASM
 void vigenereEncryptASM(char* data, size_t len, const char* key) {
     __asm {
         mov ecx, len
@@ -218,6 +369,7 @@ void vigenereEncryptASM(char* data, size_t len, const char* key) {
     }
 }
 
+// przykład szyfru Vigenère’a zaimplementowanego w C
 void vigenereDecryptC(char* data, size_t len, const char* key) {
     size_t keyLen = strlen(key);
     for (size_t i = 0; i < len; i++) {
@@ -225,6 +377,7 @@ void vigenereDecryptC(char* data, size_t len, const char* key) {
     }
 }
 
+// przykład szyfru Vigenère’a zaimplementowanego w ASM
 void vigenereDecryptASM(char* data, size_t len, const char* key) {
     __asm {
         mov ecx, len
@@ -248,70 +401,6 @@ void vigenereDecryptASM(char* data, size_t len, const char* key) {
             inc ebx
             cmp ebx, ecx
             jl vigenere_decrypt_loop
-    }
-}
-
-void substitutionEncryptC(char* data, size_t len,
-    const uint8_t* substitutionTable) {
-    for (size_t i = 0; i < len; i++) {
-        data[i] = substitutionTable[(uint8_t)data[i]];
-    }
-}
-
-void substitutionDecryptC(char* data, size_t len,
-    const uint8_t* substitutionTable) {
-    for (size_t i = 0; i < len; i++) {
-        uint8_t substitutedValue = data[i];
-
-        for (uint8_t j = 0; j < 256; j++) {
-            if (substitutionTable[j] == substitutedValue) {
-                data[i] = j;
-                break;
-            }
-        }
-    }
-}
-
-void substitutionEncryptASM(char* data, size_t len,
-    const uint8_t* substitutionTable) {
-    __asm {
-        mov ecx, len
-        mov esi, data
-        mov edi, substitutionTable
-
-        substitution_encrypt_loop :
-        movzx eax, byte ptr[esi]
-            movzx eax, byte ptr[edi + eax]
-            mov byte ptr[esi], al
-
-            inc esi
-            loop substitution_encrypt_loop
-    }
-}
-
-void substitutionDecryptASM(char* data, size_t len,
-    const uint8_t* substitutionTable) {
-    __asm {
-        mov ecx, len
-        mov esi, data
-        mov edi, substitutionTable
-
-        substitution_decrypt_loop :
-        movzx eax, byte ptr[esi]
-            xor edx, edx
-
-            search_in_table :
-        cmp byte ptr[edi + edx], al
-            je found
-            inc edx
-            cmp edx, 256
-            jl search_in_table
-
-            found :
-        mov byte ptr[esi], dl
-
-            inc esi
-            loop substitution_decrypt_loop
     }
 }
 
